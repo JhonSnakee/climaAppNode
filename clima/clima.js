e56e2166ad7ff618268d52ff9275a8b72d628b9d
JhonSnakee <56074441+JhonSnakee@users.noreply.github.com>
2020-11-11 18:12:52 -0500

    Update clima.js
    Key secret

--- clima/clima.js
+++ clima/clima.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 const getClima = async(lat, lng) => {
 
-    const key = '30c30cc091dce996bf0cff426710850a';
+    const key = '*********************************';
 
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}&units=metric`);
 
